dca4bc6561809bac605cb919190b92ad7c0fa698
root <root@kali>
2021-07-18 04:00:18 -0700

    since96 commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import{BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
-import Home from './components/pages/Home'
+
 import Merch from './components/pages/Merch';
 import Music from './components/pages/Music';
 import Contact from './components/pages/Contact';
